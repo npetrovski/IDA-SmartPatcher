5b5dcba05b99fd590e8394bc8ab4e1ce2bdaae4d
npetrovski <to.petrovski@gmail.com>
2015-01-19 01:22:42 +0200

    Initial commit

---
+++ IDASmartPatcher/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace IDASmartPatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.PatchBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.TargteBox = new System.Windows.Forms.TextBox();
+            this.patchBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.descr = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PatchBox
+            // 
+            this.PatchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PatchBox.Location = new System.Drawing.Point(99, 81);
+            this.PatchBox.Name = "PatchBox";
+            this.PatchBox.Size = new System.Drawing.Size(228, 20);
+            this.PatchBox.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(333, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TargteBox
+            // 
+            this.TargteBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TargteBox.Location = new System.Drawing.Point(99, 139);
+            this.TargteBox.Name = "TargteBox";
+            this.TargteBox.Size = new System.Drawing.Size(228, 20);
+            this.TargteBox.TabIndex = 3;
+            // 
+            // patchBtn
+            // 
+            this.patchBtn.Location = new System.Drawing.Point(315, 190);
+            this.patchBtn.Name = "patchBtn";
+            this.patchBtn.Size = new System.Drawing.Size(93, 31);
+            this.patchBtn.TabIndex = 4;
+            this.patchBtn.Text = "Patch";
+            this.patchBtn.UseVisualStyleBackColor = true;
+            this.patchBtn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Patch file (dif):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Target file:";
+            // 
+            // descr
+            // 
+            this.descr.AutoSize = true;
+            this.descr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descr.Location = new System.Drawing.Point(18, 13);
+            this.descr.Name = "descr";
+            this.descr.Size = new System.Drawing.Size(39, 20);
+            this.descr.TabIndex = 7;
+            this.descr.Text = "Text";
+            this.descr.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(22, 198);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Always on top";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(216, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 31);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Dry Run";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 247);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.descr);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.patchBtn);
+            this.Controls.Add(this.TargteBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.PatchBox);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "IDA SmartPatcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox PatchBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox TargteBox;
+        private System.Windows.Forms.Button patchBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label descr;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ IDASmartPatcher/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace IDASmartPatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[]) e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                if (file.ToLower().EndsWith("dif"))
+                {
+                    PatchBox.Text = file;
+                }
+                else
+                {
+                    TargteBox.Text = file;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Open IDA DIF file";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "Dif files (*.dif)|*.dif";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                PatchBox.Text = fdlg.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Open target file";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "All files (*.*)|*.*";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                TargteBox.Text = fdlg.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            descr.Text = "Apply IDA patch (.dif) into a target file. Just select your" + Environment.NewLine + "patch file and a target and hit \"Patch\" button.";
+            this.TopMost = checkBox1.Checked;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = checkBox1.Checked;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (PatchBox.Text == String.Empty || TargteBox.Text == String.Empty || !File.Exists(PatchBox.Text) || !File.Exists(TargteBox.Text))
+            {
+                MessageBox.Show("Invalid target file or patch.", "Error !!!");
+                return;
+            }
+
+            if (!patchFile(false))
+            {
+                DialogResult questionYN = MessageBox.Show("Target file does not match the expected byte sequence. Are you sure you want to patch?", "Warning !!!", MessageBoxButtons.YesNo);
+                if (questionYN == DialogResult.No)
+                {
+                    return;
+                }
+            }
+
+            // Apply the patch
+            if (patchFile(true))
+            {
+                MessageBox.Show("Target file has been successfully patched.", "Congratulations !!!");
+            }
+        }
+
+        protected bool patchFile(bool apply = false)
+        {
+
+            string line = "";
+            ScanFormatted scan = new ScanFormatted();
+
+            BinaryReader targetfile = new BinaryReader(File.Open(TargteBox.Text, FileMode.Open));
+            StreamReader patchfile = new System.IO.StreamReader(PatchBox.Text);
+            try
+            {
+
+
+                while ((line = patchfile.ReadLine()) != null)
+                {
+                    scan.Parse((string)line, "%x: %x %x");
+                    if (3 == scan.Results.Count)
+                    {
+                        targetfile.BaseStream.Seek(Convert.ToInt64(scan.Results[0]), SeekOrigin.Begin);
+
+                        int currentByte = targetfile.BaseStream.ReadByte();
+
+                        if (apply)
+                        {
+                            targetfile.BaseStream.WriteByte(Convert.ToByte(scan.Results[2]));
+                        } else if (currentByte != (int) Convert.ToByte(scan.Results[1])) {
+                            patchfile.Close();
+                            targetfile.Close();
+                            return false;
+                        }
+
+                    }
+                }
+
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+
+                return false;
+            }
+            finally
+            {
+                if (patchfile != null)
+                    patchfile.Close();
+                if (targetfile != null)
+                    targetfile.Close();
+
+                
+            }
+
+            return true;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            if (PatchBox.Text == String.Empty || TargteBox.Text == String.Empty || !File.Exists(PatchBox.Text) || !File.Exists(TargteBox.Text))
+            {
+                MessageBox.Show("Invalid target file or patch.", "Error !!!");
+                return;
+            }
+
+            if (patchFile(false))
+            {
+                MessageBox.Show("Target file contains the expected byte sequence. You are ready to patch.", "Congratulations !!!");
+            }
+            else
+            {
+                MessageBox.Show("Target file does not match the expected byte sequence.", "Warning !!!");
+            }
+        }
+    }
+}
